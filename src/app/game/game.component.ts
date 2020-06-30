063f4abe0b78997b0733f0e6704a74a5e4817d6d
Outsider987 <t7902195204@gmail.com>
2020-07-01 02:59:08 +0800

    Refine code and two side nav

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatMenuModule } from '@angular/material/menu';
+
+import { MatSidenav, MatDrawerToggleResult } from '@angular/material/sidenav';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-game',
@@ -8,9 +10,18 @@ import { MatMenuModule } from '@angular/material/menu';
 })
 export class GameComponent implements OnInit {
   // @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
   constructor() { }
   showFiller = false;
   ngOnInit(): void {
   }
+  toggleSideNav(drawer: MatSidenav) {
+    drawer.toggle().then((result: MatDrawerToggleResult) => {
+      console.log('選單狀態：' + result);
+    });
 
+  }
+  show(item: string) {
+    this.programName = item;
+  }
 }
