a40fa265b9f41453894e3ac03c048e07a4796fd4
Outsider987 <t7902195204@gmail.com>
2020-07-01 02:20:20 +0800

    remove unncessary code and add main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameComponent } from './game/game.component';
 import { MaterialModule } from './material/material.module';
+import { MainComponent } from './game/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
