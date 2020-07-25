d23a93db98a1f896d152c110b87163795a3ad72f
Outsider987 <t7902195204@gmail.com>
2020-07-25 19:57:10 +0800

    own validators

--- src/app/game/register/register.component.ts
+++ src/app/game/register/register.component.ts
@@ -3,40 +3,51 @@ import { FormGroup, FormControl, FormBuilder, Validators, NgForm } from '@angula
 import { first } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/api.service';
+import { UsernameValidator } from 'src/app/core/models/validatorFile';
+
+
 
 @Component({
-selector: 'app-register',
-templateUrl: './register.component.html',
-styleUrls: ['./register.component.scss']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
 })
 
 export class RegisterComponent implements OnInit {
-angForm: FormGroup;
-constructor(private fb: FormBuilder,private dataService: ApiService,private router:Router) {
-this.angForm = this.fb.group({
-email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder, private dataService: ApiService, private router: Router) {
+    this.angForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
 
-name: ['',  Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
-password: ['',  Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
-});
-}
+      name: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)]),UsernameValidator.cannotContainSpace],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+    });
+    // debugger
+    // this.angForm.setValidators(UsernameValidator.cannotContainSpace)
+  }
 
-ngOnInit() {
-}
 
-postdata(angForm1)
-{
-  this.dataService.userregistration(angForm1.value.name,angForm1.value.email,angForm1.value.password)
-  .subscribe(
-  data => {
-  //this.router.navigate(['login']);
-  },
+  ngOnInit() {
+  }
 
-  error => {
-  });
-}
+  //code snippet from https://stackoverflow.com/a/42999816/4851087
+  noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || "").trim().length === 0;
+    const isValid = !isWhitespace;
+    return (isValid ? null : { "whitespace": true });
+  }
+  postdata(angForm1) {
+    this.dataService.userregistration(angForm1.value.name, angForm1.value.email, angForm1.value.password)
+      .subscribe(
+        data => {
+          //this.router.navigate(['login']);
+        },
 
-get email() { return this.angForm.get('email'); }
-get password() { return this.angForm.get('password'); }
-get name() { return this.angForm.get('name'); }
+        error => {
+        });
+  }
+
+  get email() { return this.angForm.get('email'); }
+  get password() { return this.angForm.get('password'); }
+  get name() { return this.angForm.get('name'); }
 }
