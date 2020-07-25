80293951f2a3c333569c860d447cc6345772f7ae
Outsider987 <t7902195204@gmail.com>
2020-07-25 17:22:20 +0800

    update for invalidate from group

--- src/app/game/register/register.component.ts
+++ src/app/game/register/register.component.ts
@@ -14,10 +14,10 @@ export class RegisterComponent implements OnInit {
 angForm: FormGroup;
 constructor(private fb: FormBuilder,private dataService: ApiService,private router:Router) {
 this.angForm = this.fb.group({
-email: ['', [Validators.required
-  ,Validators.minLength(1), Validators.email]],
-password: ['', Validators.required],
-name: ['', Validators.required],
+email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
+
+name: ['',  Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+password: ['',  Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
 });
 }
 
